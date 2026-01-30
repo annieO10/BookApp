9539b19eff7d17dd3f2d29e828e8599ce147ed15
Annie <annieo10@iastate.edu>
2026-01-30 03:04:19 -0600

    test

--- MyLibraryActions.js
+++ MyLibraryActions.js
@@ -38,7 +38,7 @@ pdfInput.addEventListener("change", async (e)=>{
 });
 
 
-const url = "./pdfs/_OceanofPDF.com_Modern_Operating_Systems_5th_Edition_-_Andrew_S_Tanenbaum.pdf";
+
 pdfjsLib.GlobalWorkerOptions.workerSrc ='https://cdn.jsdelivr.net/npm/pdfjs-dist/build/pdf.worker.min.js';
 
 async function renderCover(pdf, canvas){
@@ -64,15 +64,3 @@ async function renderCover(pdf, canvas){
     await page.render({canvasContext: context, transform, viewport}).promise;    
     
 }
-async function main(){
-    const loadingTask= pdfjsLib.getDocument(saved.originalName);
-    const pdf = await loadingTask.promise;
-
-    const firstBook = document.getElementById("BookOne");
-    await renderCover(pdf, firstBook);
-
-    firstBook.addEventListener("click", () =>{
-        window.location.href = "InsertPdf.html";
-    })
-}
-main().catch((e) => console.error(e));
