9c9a37d90403a724f506acbef9c128ec71478c66
Annie <annieo10@iastate.edu>
2026-03-05 01:55:54 -0600

    w

--- MyLibraryActions.js
+++ MyLibraryActions.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
     res.send("Library API running");
